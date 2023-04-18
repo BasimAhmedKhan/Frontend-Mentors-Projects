70dde76de1eedf98041fb34632857137e452de48
Basim Khan <kbasim039@gmail.com>
2023-04-19 03:45:59 +0500

    fix: month and date issue

--- Age Calculator/script.js
+++ Age Calculator/script.js
@@ -1,7 +1,6 @@
 var dayText = document.getElementById("dayText");
 var monthText = document.getElementById("monthText");
 var yearText = document.getElementById("yearText");
-var monthsName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 calculate = () => {
     var day = document.getElementById("DAY");
@@ -9,9 +8,11 @@ calculate = () => {
     var year = document.getElementById("YEAR");
 
     var date = new Date();
-    var birthDate = new Date("April 29, 2002");
+    let dob = month.value + " " + day.value + ", " + year.value;
+    console.log(dob);
+    var birthDate = new Date(dob);
     
-    if (+day.value, +month.value, +year.value == "" || +day.value <= 31 || +month.value <= 12 || +year.value <= date.getFullYear()) {
+    if (+day.value, +month.value, +year.value == "" || +day.value > 31 || +month.value > 12 || +year.value > date.getFullYear()) {
         document.querySelectorAll(".errorMsg").forEach(e =>{
             e.classList.add("display");
         });
@@ -22,10 +23,13 @@ calculate = () => {
     else {
         var time = date.getTime() - birthDate.getTime();
         time = time / (1000 * 60 * 60 * 24);
-        dayText.innerText = Math.floor(time - (date.getTime() / (1000 * 60 * 60 * 24)));
+        let noOfDays = Math.floor(time);
         time = time / 30.417;
-        monthText.innerText = Math.floor(time - (date.getTime() / (1000 * 60 * 60 * 24 * 30.417)));
+        let noOfMonths = Math.floor(time);
         time = time / 12;
-        yearText.innerText = Math.floor(time);
+        let noOfYears = Math.floor(time);
+        dayText.innerText = noOfDays - Math.ceil(noOfMonths*30.417);
+        monthText.innerText = noOfMonths - (noOfYears*12);
+        yearText.innerText = noOfYears;
     }
 }
